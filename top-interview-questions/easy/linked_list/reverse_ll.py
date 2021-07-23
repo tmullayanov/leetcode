ef72365496f7dac4b93b7dbc60e4cdfdcb2c020f
Timur Mullayanov <tmullayanov@users.noreply.github.com>
2021-07-23 15:06:23 +0300

    [ez][ll] reverse list iteratively and recursively

--- top-interview-questions/easy/linked_list/reverse_ll.py
+++ top-interview-questions/easy/linked_list/reverse_ll.py
@@ -19,5 +19,36 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        pass
+        if head is None:
+            return None
+        cur = head
+        prev = None
+        while cur is not None:
+            _next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = _next
 
+        return prev
+
+    def reverseListRecursively(self, head: ListNode, prev=None) -> ListNode:
+        if head is None:
+            return None
+        elif head.next is None:
+            head.next = prev
+            return head
+        else:
+            new_tail = head.next
+            head.next = prev
+            new_head = self.reverseListRecursively(new_tail, prev=head)
+            return new_head
+
+
+if __name__ == '__main__':
+    lists = [
+        ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))),
+        ListNode(1),
+        ListNode(1, ListNode(2))
+    ]
+    for l in lists:
+        print(f'{l=} ans={Solution().reverseListRecursively(l)}')
