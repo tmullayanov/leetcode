39f496e619702392d18fe4bf7dce60c71822b763
Timur Mullayanov <tmullayanov@users.noreply.github.com>
2023-01-09 22:37:53 +0300

    [+] Another implementation of 3-sum (with two-pointers and no extra maps/sets)

--- top-interview-questions/medium/arrays_strings/3sum/Solution.java
+++ top-interview-questions/medium/arrays_strings/3sum/Solution.java
@@ -23,17 +23,62 @@ public class Solution {
         return new ArrayList<>(result);
     }
 
+    public List<List<Integer>> threeSumInnerPointers(int[] nums) {
+        var result = new ArrayList<List<Integer>>();
+
+        Arrays.sort(nums);
+        var n = nums.length;
+        for (int start = 0; start < n - 2; start++) {
+            if (nums[start] > 0) {
+                break;
+                // it's sorted, so we won't have -nums[start] as a sum in a right subarray
+            }
+
+            if (start > 0 && nums[start] == nums[start - 1]) {
+                continue; // we already covered this, skip
+            }
+
+            int low = start + 1, high = n - 1;
+
+            while (low < high) {
+                var sum = nums[low] + nums[high] + nums[start];
+                if (sum > 0) {
+                    high--;
+                } else if (sum < 0) {
+                    low++;
+                } else {
+                    var triplet = new ArrayList<Integer>();
+                    triplet.add(nums[start]);
+                    triplet.add(nums[low]);
+                    triplet.add(nums[high]);
+                    result.add(triplet);
+
+                    int last_low = nums[low], last_high = nums[high];
+                    while (low < high && nums[low] == last_low) {
+                        low++;
+                    }
+                    while (low < high && nums[high] == last_high) {
+                        high--;
+                    }
+
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         var s = new Solution();
 
         int[][] tests = {
                 { -1, 0, 1, 2, -1, -4 },
                 { 0, 1, 1 },
-                { 0, 0, 0 }
+                { 0, 0, 0 },
+                { 0, 0, 0, 0, 0, -1, 1 }
         };
 
         for (int[] test : tests) {
-            var result = s.threeSum(test);
+            var result = s.threeSumInnerPointers(test);
             System.out.println(result);
         }
 
